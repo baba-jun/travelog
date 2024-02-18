4f5b5d7c33b396514a7080868704f553cf01d9d8
baba-jun <149892287+baba-jun@users.noreply.github.com>
2024-02-18 08:18:02 +0000

    add mail_settings

--- travelog/settings.py
+++ travelog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from .settings_secret import *
 
 load_dotenv()
 
@@ -189,3 +190,10 @@ DEFAULT_FROM_EMAIL = 'travelogapp.official@gmail.com'
 
 # ローカルでの開発のためメールをコンソールで表示する
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'travelogapp.official@gmail.com'
+EMAIL_HOST_PASSWORD = google_mailer
+EMAIL_USE_TLS = True
---
+++ travelog/settings_secret.py
@@ -0,0 +1,1 @@
+google_mailer = 'sido yxpf xkes jpzf'
