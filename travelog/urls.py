cda9ab883c9ebbf510643da8bfac7ed57a404803
baba-jun <149892287+baba-jun@users.noreply.github.com>
2024-04-03 18:11:14 +0900

    Revert "BE#add-postdb"

--- travelog/urls.py
+++ travelog/urls.py
@@ -18,13 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import settings
+from . import views
 
 urlpatterns = [
-    path('', include('travelog_app.urls')),
+    path('', views.IndexView.as_view(), name="index"),
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- travelog_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import diary
- 
-class DiaryCreateForm(forms.ModelForm):
-    class Meta:
-        model = diary
-        fields = ('user_id', 'title', 'comment', 'country', 'prefectures','city', 'likes', 'favorites','post_images')
