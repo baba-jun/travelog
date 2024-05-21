eda73843b6482b354379e0b5a2848405f05fffab
baba-jun <msk3059i@gmail.com>
2024-05-22 00:17:40 +0900

    mod-settings

--- travelog/settings.py
+++ travelog/settings.py
@@ -14,10 +14,12 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
 
+'''
 # Amazon SES関連設定
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
 EMAIL_BACKEND = 'django_ses.SESBackend'
+'''
 
 # ロギング
 LOGGING = {
